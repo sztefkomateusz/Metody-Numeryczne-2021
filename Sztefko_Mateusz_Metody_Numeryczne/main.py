090aabdab54f3c34313059525a037b300534f600
sztefkomateusz <81700271+sztefkomateusz@users.noreply.github.com>
2021-10-06 13:01:22 +0200

    Add files via upload

---
+++ Sztefko_Mateusz_Metody_Numeryczne/main.py
@@ -0,0 +1,51 @@
+def cylinder_area(r:float,h:float):
+    """Obliczenie pola powierzchni walca. 
+    Szczegółowy opis w zadaniu 1.
+    
+    Parameters:
+    r (float): promień podstawy walca 
+    h (float): wysokosć walca
+    
+    Returns:
+    float: pole powierzchni walca 
+    """
+    return None
+
+def fib(n:int):
+    """Obliczenie pierwszych n wyrazów ciągu Fibonnaciego. 
+    Szczegółowy opis w zadaniu 3.
+    
+    Parameters:
+    n (int): liczba określająca ilość wyrazów ciągu do obliczenia 
+    
+    Returns:
+    np.ndarray: wektor n pierwszych wyrazów ciągu Fibonnaciego.
+    """
+    return None
+
+def matrix_calculations(a:float):
+    """Funkcja zwraca wartości obliczeń na macierzy stworzonej 
+    na podstawie parametru a.  
+    Szczegółowy opis w zadaniu 4.
+    
+    Parameters:
+    a (float): wartość liczbowa 
+    
+    Returns:
+    touple: krotka zawierająca wyniki obliczeń 
+    (Minv, Mt, Mdet) - opis parametrów w zadaniu 4.
+    """
+    return None
+
+def custom_matrix(m:int, n:int):
+    """Funkcja zwraca macierz o wymiarze mxn zgodnie 
+    z opisem zadania 7.  
+    
+    Parameters:
+    m (int): ilość wierszy macierzy
+    n (int): ilość kolumn macierzy  
+    
+    Returns:
+    np.ndarray: macierz zgodna z opisem z zadania 7.
+    """
+    return None
